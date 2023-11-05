bb76cc22893423146d3163f22ad1bfca4f39b12d
Marius Meskauskas (uif06769) <marius.meskauskas@continental-corporation.com>
2023-11-05 05:35:28 +0200

    added enum to menu, need to test

--- Rastaurant_App/Program.cs
+++ Rastaurant_App/Program.cs
@@ -1,37 +1,56 @@
-﻿namespace Rastaurant_App
+﻿using System.Threading.Channels;
+
+namespace Rastaurant_App
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-            RestaurantService restaurant = new RestaurantService();
+			RestaurantService restaurant = new RestaurantService();
 
-            while (true)
-            {
-                Console.WriteLine("Restaurant Service Menu:");
-                Console.WriteLine("1. Register Order");
-                Console.WriteLine("2. Mark Table as Vacant");
-                Console.WriteLine("3. Exit");
+			while (true)
+			{
+				Console.WriteLine(@"
+					Restaurant Service Menu:
+                                    1. Register Order
+                                    2. Mark Table as Vacant
+                                    3. Print Checkout
+                                    4. Exit");
 
-                Console.Write("Select an option: ");
-                string choice = Console.ReadLine();
+				Console.Write("Select an option: ");
 
-                switch (choice)
-                {
-                    case "1":
-                        restaurant.RegisterOrder();
-                        break;
-                    case "2":
-                        restaurant.MarkTableAsVacant();
-                        break;
-                    case "3":
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Invalid option. Please try again.");
-                        break;
-                }
-            }
-        }
+				if (!Int32.TryParse(Console.ReadLine(), out int choice))
+				{
+					Console.WriteLine("Input is not a number!");
+				}
+
+				switch ((MenuOptions)choice)
+				{
+					case MenuOptions.RegisterOrder:
+						restaurant.RegisterOrder();
+						break;
+					case MenuOptions.MarkTableAsVacant:
+						restaurant.MarkTableAsVacant();
+						break;
+					case MenuOptions.PrintCheck:
+						//Method to print checkout;
+						break;
+					case MenuOptions.Exit:
+						Environment.Exit(0);
+						break;
+					default:
+						Console.WriteLine("Invalid option. Please try again.");
+						break;
+				}
+			}
+		}
+
+		public enum MenuOptions
+		{
+			RegisterOrder = 1,
+			MarkTableAsVacant = 2,
+			PrintCheck = 3,
+			Exit = 4
+		}
 	}
 }
